df2e385a679a22d31a02e32266a08317559240b2
freemanchrislewis <flewi017@fiu.edu>
2024-07-22 14:34:04 -0400

    Initial commit

--- publish_quarto.R
+++ publish_quarto.R
@@ -7,7 +7,6 @@ if (!require("quarto")) {
 library(quarto)
 
 # Set the working directory to your Quarto project
-# Replace this with the path to your project directory
 setwd("/Users/freemanlewis/Desktop/Github/Quarto Documents/")
 
 # Render the Quarto document
@@ -15,22 +14,21 @@ quarto_render("FreemanLewis_Resume_20240722.qmd")
 
 # Initialize a Git repository if not already initialized
 system("git init")
+
+# Add and commit files
 system("git add .")
 system('git commit -m "Initial commit"')
 
-# Add the remote GitHub repository
-system('git remote add origin https://github.com/freemanchrislewis/FreemanLewis_Resume.git')
-
-# Check the current branch name
+# Check the current branch name and switch to main if necessary
 branch <- system("git branch --show-current", intern = TRUE)
 if (branch == "") {
-  # If no branch is set, create and switch to 'main' branch
   system("git checkout -b main")
-  branch <- "main"
 } else if (branch != "main") {
-  # If the current branch is not 'main', switch to 'main' branch
   system("git checkout main")
 }
+
+# Verify the remote URL
+system("git remote -v")
 
 # Push the files to the main branch of your remote repository
 system("git push -u origin main")
